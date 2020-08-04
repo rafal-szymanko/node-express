c6578f4554df66916841bd4d122c1f79b4dfc862
rafal-szymanko <61496255+rafal-szymanko@users.noreply.github.com>
2020-08-04 19:52:23 +0200

    Add routing in Express

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const port = 3000;
+const isAdmin = () => {
+    return false;
+};
+
+app.use((req, res, next) => {
+    res.show = (name) => {
+        res.sendFile(path.join(__dirname, `/views/${name}`));
+    };
+    next();
+});
+
+
+app.use('/admin', (req, res, next) => {
+    if(isAdmin()) next();
+    else res.show('forbidden.html');
+  });
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.show('home.html');
+});
+
+app.get('/home', (req, res) => {
+    res.show('home.html');
+});
+
+app.get('/about', (req, res) => {
+    res.show('about.html');
+});
+
+app.use((req, res) => {
+    res.status(404).show('404.html');
+  })
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
+});
